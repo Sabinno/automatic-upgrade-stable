c36fb5d4124ae903b8f055d3f8a184fdc6bce573
Evan Sabin <evan.parker.sabin@gmail.com>
2021-09-06 20:55:51 -0400

    Formatting and comments

--- get-upgrade-cand.py
+++ get-upgrade-cand.py
@@ -1,11 +1,12 @@
 import dnf
- 
+
+# Initialize dnf
 base = dnf.Base()
 base.read_all_repos()
 base.fill_sack()
- 
 
+# Gets current upgrade candidates
 for pkg in list(base.sack.query().upgrades()):
     print(pkg.name, pkg.version)
- 
+
 base.close()
