d54f0e1e7259c9dce82209f4620eb5ba8f67840f
Evan Sabin <evan.parker.sabin@gmail.com>
2021-10-07 20:34:16 -0400

    Made script actually work

--- get-upgrade-cand.py
+++ get-upgrade-cand.py
@@ -1,7 +1,8 @@
 # This script aims to improve system stability by only upgrading packages that have not seen further upgrades since the last time this script was run.
-# For best results, only run as superuser, ideally once-weekly or thereabout via cron.
+# For best results, run as superuser (required to install system packages via DNF), ideally once-weekly or thereabout.
 
 import dnf
+from pathlib import Path
 import os.path
 import numpy
 import csv
@@ -20,20 +21,23 @@ def current_upgrade_candidates():
 
 # Save upgrade candidates to file
 def save_upgrade_candidates():
-    with open(os.path.join(last_upgrade_candidates_dir, "last-upgrade-candidates.csv"), 'w', newline='') as file
+    with open(os.path.join(last_upgrade_candidates_dir, "last-upgrade-candidates.csv"), 'w', newline='') as file:
         csvwriter = csv.writer(file, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writerows(current_upgrade_candidates())
+        csvwriter.writerows(current_upgrade_candidates())
 
 # Open results of last save_upgrade_candidates
 def last_upgrade_candidates():
-    with open(os.path.join(last_upgrade_candidates_dir, "last-upgrade-candidates.csv"), 'r', newline='') as file
-        csvreader = csv.reader(file, delimiter=' ', quotechar='|')
-        return [for row in csvreader]
-         
+    try:
+        with open(os.path.join(last_upgrade_candidates_dir, "last-upgrade-candidates.csv"), 'r', newline='') as file:
+            csvreader = csv.reader(file, delimiter=' ', quotechar='|')
+            return [tuple(row) for row in csvreader]
+    except:
+        return
+
 
 # Upgrade "stable packages" based on comparing the currently available upgrades to what was available 1 week ago. 
 if os.path.isfile(last_upgrade_candidates_dir) and os.path.getsize(last_upgrade_candidates_dir) > 0:
-    base.install(intersection(current_upgrade_candidates(), last_upgrade_candidates())
+    base.install([elem[0] for elem in set(current_upgrade_candidates()).intersection(last_upgrade_candidates())])
     base.resolve()
     base.download_packages(base.transaction.install_set)
     base.do_transaction()
